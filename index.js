7fe3c5ac3712185df29eee8794c706b9978c0e57
Anil Kishan <kishan.anil@gmail.com>
2019-05-22 00:51:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-
+function theBeatlesPlay(musicians, instruments){
+  var empty = []
+  for (var i=0 ; i < musicians.length; i++) {
+    empty.push(empty[i]) + " plays " + instruments[i]  
+  }
+  return empty
+}
 
 
 function theBeatlesPlay(players, instruments) {
