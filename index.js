81181f7fd1491991a066a359bef7039ee8edccba
Anil Kishan <kishan.anil@gmail.com>
2019-05-22 00:58:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(facts) {
-  while (var i=0; i < musicians.length; i++)
+  while (var i=0; i < facts.length; i++) {
+    facts.push(facts[i] + " !!!")
+  }
+  return facts
 }
