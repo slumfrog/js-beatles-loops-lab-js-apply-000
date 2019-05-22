842f571d26b6a212c72eeb4d6bc2af702f673f2e
Anil Kishan <kishan.anil@gmail.com>
2019-05-22 01:19:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 
-function iLoveTheBeatles(num) {
 
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
-
 function iLoveTheBeatles(num) {
     var love = [];
     do {
