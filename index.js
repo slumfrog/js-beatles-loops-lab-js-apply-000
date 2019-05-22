2e599d8799dc30e8225001446e052361b47bfb31
Anil Kishan <kishan.anil@gmail.com>
2019-05-22 00:47:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var empty = []
   var arrayLength = theBeatlesPlay.arrayLength
-  for (var i = ; i < arrayLength; i++) 
+  for (var i = ; i < arrayLength; i++) {
+    
+  }
 }
